c3dd789053bb92a50af02c9d3b6a9857722fa943
Rachel Anne R Brillantes <momoxielle@gmail.com>
2017-10-26 20:56:40 +0800

    Deleted files

--- Application/back-end/basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mi152_153_00_master_db',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
