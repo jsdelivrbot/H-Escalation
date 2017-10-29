44737e109463a55714258ad4ac279230bd6d5724
Watz Alconcel <watzalconcel@gmail.com>
2017-10-30 07:58:29 +0800

    published app files

---
+++ EMM Mobile App/src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebasConfig = {
+apiKey: "AIzaSyCoMPal2i5tNA_V0FBG83ueBn1cLDrEvBs",
+    authDomain: "my-firebaseapp-d1813.firebaseapp.com",
+    databaseURL: "https://my-firebaseapp-d1813.firebaseio.com",
+    projectId: "my-firebaseapp-d1813",
+    storageBucket: "my-firebaseapp-d1813.appspot.com",
+    messagingSenderId: "14625819387"
+
+};
